dcd08204176fd5d4e68abb53ec20c47b45e1e8e9
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-01-09 18:39:29 +0300

    Create database from docker-compose.yaml and application.yml files

---
+++ src/main/java/kz/zhelezyaka/SpringExample/entityes/User.java
@@ -0,0 +1,23 @@
+package kz.zhelezyaka.SpringExample.entityes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
