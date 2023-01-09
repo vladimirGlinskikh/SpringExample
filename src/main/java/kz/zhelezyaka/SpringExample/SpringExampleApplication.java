31de237b54322d85489cee1bdc5f2fbd23dd4209
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-01-09 17:04:44 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/SpringExample/SpringExampleApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.SpringExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/zhelezyaka/SpringExample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.SpringExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
